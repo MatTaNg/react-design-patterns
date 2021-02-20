ed4bd9e87a2b1e37eddd786f88ea62f82265fa3b
MatTaNg <ng5280@hotmail.com>
2021-02-19 23:07:53 -0500

    Adding compound components and stateReducer patterns

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ConsumeStateReducer from './stateReducer.js';
+import ConsumeCompoundComponent from './compoundComponent.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      <div className="gridCell">
+        <div className="container">
+          <p>State Reducer Pattern:</p> <ConsumeStateReducer />
+        </div>
+      </div>
+      <div className="gridCell">
+        <div className="container">
+          <p>Compound Components:</p> <ConsumeCompoundComponent />
+        </div>
+      </div>
     </div>
   );
 }
